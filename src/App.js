3b872b1c4a6f2711fa90e8675073da38be30be39
SergeyShurekov <shurekov@gmail.com>
2022-10-29 19:11:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+import { CssBaseline } from '@mui/material';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+
+      <div className="App">
+        <header className="App-header">
+          <h1>Вывод бизнеса онлайн</h1>
+          <p>
+            Если в интернет не выйдешь ты, выйдет твой конкурент
+          </p>
+        </header>
+        <div>
+          Cards
+          <Button variant='contained'>Заказать</Button>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
